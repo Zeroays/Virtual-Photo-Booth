aef31f0cbc59b01c1b19cf088b4a5108ab21de9b
Vasu Rabaib <vrabaib@gmail.com>
2020-10-12 14:21:15 -0700

    Fixed NavBar and SideBar Components - brief manual visual testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "/src/components/NavBar/NavBar";
+import SideBar from "/src/components/SideBar/SideBar";
 
 const App = () => {
   return (
     <div className="main-wrapper">
       <NavBar />
+      <SideBar />
     </div>
   );
 };
