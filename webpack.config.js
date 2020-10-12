1280b3d0a81e72f05c423751c6241c6fe4d3fef1
Vasu Rabaib <vrabaib@gmail.com>
2020-10-12 12:20:50 -0700

    Commiting Boilerplate Files -> No App Content

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
