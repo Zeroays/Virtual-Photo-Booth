6819fd2a33ddb63f56cd42b9119590d4417bea68
Vasu Rabaib <vrabaib@gmail.com>
2020-10-12 17:10:11 -0700

    Git checkout NavBar folder/content from master -> changed logo file path

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-import logo from "../../assets/images/camera_logo.png";
+import logo from "../../assets/icons/camera_logo.png";
 
 const NavBar = () => {
   return (
