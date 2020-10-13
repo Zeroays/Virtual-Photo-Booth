d81e74cffd8aa566b99265c0ff9e52b7d3863478
Vasu Rabaib <vrabaib@gmail.com>
2020-10-12 18:40:40 -0700

    Git checkout Props Folder from master

---
+++ src/components/PropertiesBar/Props/PropData.js
@@ -0,0 +1,189 @@
+const photoPropsData = [
+  {
+    type: "Foods",
+    label: "Apple",
+    img: "/src/assets/props/apple.png",
+  },
+  {
+    type: "Headwear",
+    label: "Bunny Ears",
+    img: "/src/assets/props/bunny_ears.png",
+  },
+  {
+    type: "Superhero",
+    label: "Captain America Shield",
+    img: "/src/assets/props/captain_america_shield.png",
+  },
+  {
+    type: "Headwear",
+    label: "Cat Ears",
+    img: "/src/assets/props/cat_ears.png",
+  },
+  {
+    type: "Animal",
+    label: "Cat",
+    img: "/src/assets/props/cat.png",
+  },
+  {
+    type: "Speech",
+    label: "Cloud Speech Bubble",
+    img: "/src/assets/props/cloud_speech_bubble.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Coin Stack",
+    img: "/src/assets/props/coin_stack.png",
+  },
+  {
+    type: "Headwear",
+    label: "Crown",
+    img: "/src/assets/props/crown.png",
+  },
+  {
+    type: "Animal",
+    label: "Dog",
+    img: "/src/assets/props/dog.png",
+  },
+  {
+    type: "Headwear",
+    label: "Glasses",
+    img: "/src/assets/props/glasses.png",
+  },
+  {
+    type: "Headwear",
+    label: "Goatee",
+    img: "/src/assets/props/goatee.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Heart",
+    img: "/src/assets/props/heart.png",
+  },
+  {
+    type: "Superhero",
+    label: "Infinity Gauntlet",
+    img: "/src/assets/props/infinity_gauntlet.png",
+  },
+  {
+    type: "Superhero",
+    label: "Ironman Helmet",
+    img: "/src/assets/props/ironman_helmet.png",
+  },
+  {
+    type: "Headwear",
+    label: "Lips",
+    img: "/src/assets/props/lips.png",
+  },
+  {
+    type: "Speech",
+    label: "Lol Speech Bubble",
+    img: "/src/assets/props/lol_speech_bubble.png",
+  },
+  {
+    type: "Headwear",
+    label: "Mask",
+    img: "/src/assets/props/mask.png",
+  },
+  {
+    type: "Headwear",
+    label: "MLG Eyewear",
+    img: "/src/assets/props/MLG_eyewear.png",
+  },
+  {
+    type: "Headwear",
+    label: "Mustache",
+    img: "/src/assets/props/mustache.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Ninja Star",
+    img: "/src/assets/props/ninja_star.png",
+  },
+  {
+    type: "Speech",
+    label: "OK Speech Bubble",
+    img: "/src/assets/props/ok_speech_bubble.png",
+  },
+  {
+    type: "Speech",
+    label: "OMG Speech Bubble",
+    img: "/src/assets/props/omg_speech_bubble.png",
+  },
+  {
+    type: "Foods",
+    label: "Pineapple",
+    img: "/src/assets/props/pineapple.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Popsicle",
+    img: "/src/assets/props/popsicle.png",
+  },
+  {
+    type: "Holiday",
+    label: "Present",
+    img: "/src/assets/props/present.png",
+  },
+  {
+    type: "Holiday",
+    label: "Pumpkin",
+    img: "/src/assets/props/pumpkin.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Rainbow",
+    img: "/src/assets/props/rainbow.png",
+  },
+  {
+    type: "Speech",
+    label: "Rectangle Speech Bubble",
+    img: "/src/assets/props/rectangle_speech_bubble.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Ribbon",
+    img: "/src/assets/props/ribbon.png",
+  },
+  {
+    type: "Holiday",
+    label: "Santa Hat",
+    img: "/src/assets/props/santa_hat.png",
+  },
+  {
+    type: "Headwear",
+    label: "Sleeping Mask",
+    img: "/src/assets/props/sleeping_mask.png",
+  },
+  {
+    type: "Speech",
+    label: "Surprise Speech Bubble",
+    img: "/src/assets/props/surprise_speech_bubble.png",
+  },
+  {
+    type: "Food",
+    label: "Swirl Lollipop",
+    img: "/src/assets/props/swirl_lollipop.png",
+  },
+  {
+    type: "Miscellaneous",
+    label: "Thumbs Up",
+    img: "/src/assets/props/thumbs_up.png",
+  },
+  {
+    type: "Food",
+    label: "Watermellon",
+    img: "/src/assets/props/watermellon.png",
+  },
+  {
+    type: "Superhero",
+    label: "Wonder Woman Headband",
+    img: "/src/assets/props/wonder_woman_headband.png",
+  },
+  {
+    type: "Speech",
+    label: "Wow Speech Bubble",
+    img: "/src/assets/props/wow_speech_bubble.png",
+  },
+];
+
+export default photoPropsData;
