0fd863992c3adb4399627bcf277d4f243c07f473
Vasu Rabaib <vrabaib@gmail.com>
2020-10-12 19:50:19 -0700

    Adding Canvas Files

---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./canvas.css";
+
+const Canvas = () => {
+  return (
+    <div class="canvas">
+      <div class="canvas-image-content">
+        <img src="Conference.jpg" alt="conference" />
+      </div>
+    </div>
+  );
+};
+
+export default Canvas;
