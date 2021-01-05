36f556f53dd3779b235e8b1f9fe927c3fa7fa012
Vasu Rabaib <vrabaib@gmail.com>
2021-01-04 22:08:34 -0500

    Changed Prop Data - testing speed

--- src/components/PropertiesBar/Props/PropData.js
+++ src/components/PropertiesBar/Props/PropData.js
@@ -1,53 +1,91 @@
+import Apple from "/src/assets/props/apple.png";
+import BunnyEars from "/src/assets/props/bunny_ears.png";
+import CaptainAmericaShield from "/src/assets/props/captain_america_shield.png";
+import CatEars from "/src/assets/props/cat_ears.png";
+import Cat from "/src/assets/props/cat.png";
+import CloudSpeechBubble from "/src/assets/props/cloud_speech_bubble.png";
+import CoinStack from "/src/assets/props/coin_stack.png";
+import Crown from "/src/assets/props/crown.png";
+import Dog from "/src/assets/props/dog.png";
+import Glasses from "/src/assets/props/glasses.png";
+import Goatee from "/src/assets/props/goatee.png";
+import Heart from "/src/assets/props/heart.png";
+import InfinityGauntlet from "/src/assets/props/infinity_gauntlet.png";
+import IronmanHelmet from "/src/assets/props/ironman_helmet.png";
+import Lips from "/src/assets/props/lips.png";
+import LolSpeechBubble from "/src/assets/props/lol_speech_bubble.png";
+import Mask from "/src/assets/props/mask.png";
+import MlgEyewear from "/src/assets/props/MLG_eyewear.png";
+import Mustache from "/src/assets/props/mustache.png";
+import NinjaStar from "/src/assets/props/ninja_star.png";
+import OkSpeechBubble from "/src/assets/props/ok_speech_bubble.png";
+import OmgSpeechBubble from "/src/assets/props/omg_speech_bubble.png";
+import Pineapple from "/src/assets/props/pineapple.png";
+import Popsicle from "/src/assets/props/popsicle.png";
+import Present from "/src/assets/props/present.png";
+import Pumpkin from "/src/assets/props/pumpkin.png";
+import Rainbow from "/src/assets/props/rainbow.png";
+import RectangleSpeechBubble from "/src/assets/props/rectangle_speech_bubble.png";
+import Ribbon from "/src/assets/props/ribbon.png";
+import SantaHat from "/src/assets/props/santa_hat.png";
+import SleepingMask from "/src/assets/props/sleeping_mask.png";
+import SurpriseSpeechBubble from "/src/assets/props/surprise_speech_bubble.png";
+import SwirlLollipop from "/src/assets/props/swirl_lollipop.png";
+import ThumbsUp from "/src/assets/props/thumbs_up.png";
+import Watermellon from "/src/assets/props/watermellon.png";
+import WonderWomanHeadband from "/src/assets/props/wonder_woman_headband.png";
+import WowSpeechBubble from "/src/assets/props/wow_speech_bubble.png";
+
 const photoPropsData = [
   {
     type: "Foods",
     label: "Apple",
-    img: "/src/assets/props/apple.png",
+    img: Apple,
   },
   {
     type: "Headwear",
     label: "Bunny Ears",
-    img: "/src/assets/props/bunny_ears.png",
+    img: BunnyEars,
   },
   {
     type: "Superhero",
     label: "Captain America Shield",
-    img: "/src/assets/props/captain_america_shield.png",
+    img: CaptainAmericaShield,
   },
   {
     type: "Headwear",
     label: "Cat Ears",
-    img: "/src/assets/props/cat_ears.png",
+    img: CatEars,
   },
   {
     type: "Animal",
     label: "Cat",
-    img: "/src/assets/props/cat.png",
+    img: Cat,
   },
   {
     type: "Speech",
     label: "Cloud Speech Bubble",
-    img: "/src/assets/props/cloud_speech_bubble.png",
+    img: CloudSpeechBubble,
   },
   {
     type: "Miscellaneous",
     label: "Coin Stack",
-    img: "/src/assets/props/coin_stack.png",
+    img: CoinStack,
   },
   {
     type: "Headwear",
     label: "Crown",
-    img: "/src/assets/props/crown.png",
+    img: Crown,
   },
   {
     type: "Animal",
     label: "Dog",
-    img: "/src/assets/props/dog.png",
+    img: Dog,
   },
   {
     type: "Headwear",
     label: "Glasses",
-    img: "/src/assets/props/glasses.png",
+    img: Glasses,
   },
   {
     type: "Headwear",
