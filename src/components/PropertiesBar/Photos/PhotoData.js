b4e23048897e766e3a1d744ce9bac563cc978321
Vasu Rabaib <vrabaib@gmail.com>
2021-01-07 19:25:24 -0500

    Changed photoData to disclude 2 more large images; testing web load performance

--- src/components/PropertiesBar/Photos/PhotoData.js
+++ src/components/PropertiesBar/Photos/PhotoData.js
@@ -4,14 +4,14 @@ import Dog from "/src/assets/stockPhotos/dog.jpg";
 import WomanSitting from "/src/assets/stockPhotos/woman_sitting.jpg";
 import Squirrel from "/src/assets/stockPhotos/squirrel.jpg";
 import Bird from "/src/assets/stockPhotos/bird.jpg";
-import Conference from "/src/assets/stockPhotos/conference.jpg";
+// import Conference from "/src/assets/stockPhotos/conference.jpg";
 // import CrowdedTrainStation from "/src/assets/stockPhotos/crowded_train_station.jpg";
 // import GroupGettingIceCream from "/src/assets/stockPhotos/group_getting_icecream.jpg";
 // import GuyInForest from "/src/assets/stockPhotos/guy_in_forest.jpg";
 import GuyWithShades from "/src/assets/stockPhotos/guy_with_shades.jpg";
 // import InsideTrain from "/src/assets/stockPhotos/inside_train.jpg";
 import KittenInBox from "/src/assets/stockPhotos/kitten_in_box.jpg";
-import OutdoorConversation from "/src/assets/stockPhotos/outdoor_conversation.jpg";
+// import OutdoorConversation from "/src/assets/stockPhotos/outdoor_conversation.jpg";
 
 const photoData = [
   Dog,
@@ -20,14 +20,14 @@ const photoData = [
   Skateboarder,
   CatDog,
   Bird,
-  Conference,
+  // Conference,
   // CrowdedTrainStation,
   // GroupGettingIceCream,
   // GuyInForest,
   GuyWithShades,
   // InsideTrain,
   KittenInBox,
-  OutdoorConversation,
+  // OutdoorConversation,
 ];
 
 export default photoData;
