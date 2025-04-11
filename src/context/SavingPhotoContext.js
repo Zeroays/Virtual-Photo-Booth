987e1ad8a1d908178dd86bad0d121ebaf20f2349
Vasu Rabaib <vrabaib@gmail.com>
2025-04-11 16:06:47 -0400

    Adding working ContextAPI for saving photo

---
+++ src/context/SavingPhotoContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const SavingPhotoContext = createContext();
+
+export const SavingPhotoContextProvider = ({ children }) => {
+    const [savingPhoto, setSavingPhoto] = useState(false);
+
+    return (
+        <SavingPhotoContext.Provider value={{ savingPhoto, setSavingPhoto }}>
+            {children}
+        </SavingPhotoContext.Provider>
+    );
+};
+
+export const useSavingPhotoContext = () => {
+    const savingPhotoContext = useContext(SavingPhotoContext);
+    return savingPhotoContext;
+};
