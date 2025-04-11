cc4c5b5b5ab2a318daab30f12bdda280f3110ae2
Vasu Rabaib <vrabaib@gmail.com>
2025-04-10 20:07:14 -0400

    Added PropertyContext to change Sidebar content using ContextAPI

---
+++ src/context/PropertyContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from "react";
+
+export const PropertyContext = createContext();
+
+export const PropertyContextProvider = ({ children }) => {
+    const [currentProperty, setCurrentProperty] = useState("Photos");
+
+    return (
+        <PropertyContext.Provider value={{ currentProperty, setCurrentProperty }}>
+            {children}
+        </PropertyContext.Provider>
+    );
+}
+
+export const usePropertyContext = () => {
+    const propertyContext = useContext(PropertyContext);
+    return propertyContext;
+}
